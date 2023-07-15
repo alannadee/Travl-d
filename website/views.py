f6be6915116c5365d6f7c2c0ddd218d6012bbc60
alannadee <139197719+alannadee@users.noreply.github.com>
2023-07-15 18:13:29 +1200

    1.6
    Account page created (followed Mr Glasse's tutorial)

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
+import os
+from pathlib import Path
+from PIL import Image 
+import secrets
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Post, User, Comment, Like
 from . import db
+from .forms import UdateAccountForm, RegistrationForm
+
 
 views = Blueprint("views", __name__)
 
@@ -123,3 +129,36 @@ def like(post_id):
         db.session.commit()
 
     return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
+
+def save_picture(form_picture):
+    path = Path("website/static/profile_pics")
+    random_hex = secrets.token_hex(8)
+    _,f_ext = os.path.splitext(form_picture.filename) 
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(path, picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@views.route("/account", methods=['GET','POST'])
+@login_required
+def account():
+    form = UdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated')
+        return redirect(url_for('views.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
+    return render_template('account.html', user=current_user, image_file=image_file, form=form)
+    
